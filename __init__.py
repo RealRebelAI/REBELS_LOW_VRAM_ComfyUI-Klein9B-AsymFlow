b90466dda7bb31e25d4476732f24b092e0706958
RealRebelAI <noahpmina@gmail.com>
2026-05-15 17:38:22 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,11 +1,17 @@
-from .nodes import AsymFlux2KleinLoader, AsymFlux2KleinSampler
-from .nodes import AsymFlux2KleinLoaderNoCLIP, AsymFlux2KleinCondSampler
+from .nodes import (
+    AsymFlux2KleinLoader,
+    AsymFlux2KleinSampler,
+    AsymFlux2KleinLoaderNoCLIP,
+    AsymFlux2KleinCondSampler,
+    AsymFlux2KleinLoaderGGUF
+)
 
 NODE_CLASS_MAPPINGS = {
     "AsymFlux2KleinLoader": AsymFlux2KleinLoader,
     "AsymFlux2KleinSampler": AsymFlux2KleinSampler,
     "AsymFlux2KleinLoaderNoCLIP": AsymFlux2KleinLoaderNoCLIP,
     "AsymFlux2KleinCondSampler": AsymFlux2KleinCondSampler,
+    "AsymFlux2KleinLoaderGGUF": AsymFlux2KleinLoaderGGUF,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
@@ -13,6 +19,7 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "AsymFlux2KleinSampler": "AsymFLUX.2 Klein Sampler",
     "AsymFlux2KleinLoaderNoCLIP": "AsymFLUX.2 Klein Loader (No CLIP)",
     "AsymFlux2KleinCondSampler": "AsymFLUX.2 Klein Cond Sampler",
+    "AsymFlux2KleinLoaderGGUF": "AsymFLUX.2 Klein Loader (GGUF)",
 }
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
